b83e649d4420ddaf72aab70fdfff4fb55236d017
ThsSantos <ths3667@gmail.com>
2021-09-15 20:58:29 -0300

    criação base js

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-// const isbn = 9788501091338
+document.querySelector('button').addEventListener('click', busca)
+const div = document.querySelector('.result')
 
 
+function busca() {
+  let nomeTime = document.querySelector('input').value
 
-fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:9788554513566').then(function (resposta) {
-  resposta.json().then(function (data) {
-    console.log(data)
-  })
-})
+  if (nomeTime == "") {
+
+  } else {
+
+    fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + nomeTime).then(function (resposta) {
+      resposta.json().then(function (data) {
+        // console.log(data)
+
+        for (dados of data.teams) {
+          console.log(dados)
+          let divTime = document.createElement('div')
+          divTime.setAttribute('class', 'time')
+          let img = document.createElement('img')
+          img.setAttribute('src', dados.strTeamLogo)
+          let p = document.createElement('p')
+          p.innerText = dados.strTeam
+          // console.log(dados.strTeam)
+          divTime.appendChild(img)
+          divTime.appendChild(p)
+          div.appendChild(divTime)
+
+        }
+
+      })
+    })
+  }
+
+
+}
+
