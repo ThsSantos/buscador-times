4f8524f69e604fcf635d7fa92a055f2448f92b31
Thiago Santos <ths3667@gmail.com>
2021-09-16 11:59:40 -0300

    mesclando html com js

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ document.querySelector('button').addEventListener('click', busca)
 const div = document.querySelector('.result')
 
 
+
 function busca() {
-  let nomeTime = document.querySelector('input').value
+  const nomeTime = document.querySelector('input').value
+  div.innerHTML = ""
 
   if (nomeTime == "") {
 
   } else {
 
-    fetch('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + nomeTime).then(function (resposta) {
+    fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${nomeTime}`).then(function (resposta) {
       resposta.json().then(function (data) {
         // console.log(data)
 
@@ -21,9 +23,13 @@ function busca() {
           img.setAttribute('src', dados.strTeamLogo)
           let p = document.createElement('p')
           p.innerText = dados.strTeam
+          let botao = document.createElement('button')
+          botao.innerText = "Saiba Mais +"
+          botao.setAttribute('onclick', `time(${dados.idTeam})`)
           // console.log(dados.strTeam)
           divTime.appendChild(img)
           divTime.appendChild(p)
+          divTime.appendChild(botao)
           div.appendChild(divTime)
 
         }
