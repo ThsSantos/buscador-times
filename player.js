ed78f096db81f2f676de65d23db241dcf91576ee
Thiago Santos <ths3667@gmail.com>
2021-09-23 12:01:31 -0300

    primeiros itens pagina jogador

--- player.js
+++ player.js
@@ -21,23 +21,24 @@ function busca() {
           img.setAttribute('src', dados.strCutout)
           let p = document.createElement('p')
           let esporte = document.createElement('p')
-          let liga = document.createElement('p')
+          let time = document.createElement('p')
           p.innerText = dados.strPlayer
           esporte.innerText = `Esporte: ${dados.strSport}`
-          if (dados.strLeague == "") {
-            liga.innerText = "Liga: Indefinida"
-          } else {
-            liga.innerText = `Liga: ${dados.strLeague}`
+          if(dados.strTeam != null || dados.strTeam != ""){
+            time.innerText = `Time: ${dados.strTeam}`
+          }else{
+            time.innerText = `Time: Time não encontrado`
           }
 
           let botao = document.createElement('button')
           botao.innerText = "Saiba Mais +"
-          botao.setAttribute('onclick', `time(${dados.idTeam})`)
+          botao.setAttribute('onclick', `jogador(${dados.idPlayer})`)
           // console.log(dados.strTeam)
           divJogador.appendChild(img)
           divJogador.appendChild(p)
+          divJogador.appendChild(time)
           divJogador.appendChild(esporte)
-          divJogador.appendChild(liga)
+          
           divJogador.appendChild(botao)
           div.appendChild(divJogador)
 
@@ -48,4 +49,58 @@ function busca() {
   }
 
 
+}
+
+function jogador(id){
+  div.innerHTML = ""
+  fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupplayer.php?id=${id}`).then(function(infos){
+    infos.json().then(function(dados){
+      console.log(dados)
+      for(player of dados.players){
+        let divPlayer = document.createElement('div')
+        divPlayer.setAttribute('class','player')
+        let nome = document.createElement('h1')
+        nome.innerText = player.strPlayer
+        let foto = document.createElement('p')
+        if(player.strThumb != null){
+          let img = document.createElement('img')
+          img.setAttribute('src', player.strThumb)
+          foto.appendChild(img)
+        }else{
+          foto.setAttribute('class','erro')
+          foto.innerText = "Foto não encontrada"
+        }
+
+        let altura = document.createElement('p')
+        altura.innerText = `Altura: ${player.strHeight}`
+        let peso = document.createElement('p')
+        peso.innerText = `Peso: ${player.strWeight}`
+        let posição = document.createElement('p')
+        posição.innerText = `Posição: ${player.strPosition}`
+        let time = document.createElement('p')
+        time.innerText = `Time: ${player.strTeam}`
+        let nascimento = document.createElement('p')
+        nascimento.innerText = `Data de nascimento: ${player.dateBorn}`
+        let localNas = document.createElement('p')
+        localNas.innerText = `Local de nascimento: ${player.strBirthLocation}`
+        let nacionalidade = document.createElement('p')
+        nacionalidade.innerText = `Nacionalidade: ${player.strNationality}`
+
+
+        divPlayer.appendChild(nome)
+        divPlayer.appendChild(foto)
+        divPlayer.appendChild(time)
+        divPlayer.appendChild(posição)
+        divPlayer.appendChild(altura)
+        divPlayer.appendChild(peso)
+        divPlayer.appendChild(nascimento)
+        divPlayer.appendChild(localNas)
+        divPlayer.appendChild(nacionalidade)
+        
+
+        div.appendChild(divPlayer)
+      }
+    })
+  })
+  
 }
